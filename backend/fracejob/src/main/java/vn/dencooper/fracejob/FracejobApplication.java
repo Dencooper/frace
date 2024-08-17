c30b7fbe884066eb5373584aa95c68c99c9e9929
namle <lephuongnam958506@gmail.com>
2024-08-17 13:31:22 +0700

    "initalize backend"

---
+++ backend/fracejob/src/main/java/vn/dencooper/fracejob/FracejobApplication.java
@@ -0,0 +1,13 @@
+package vn.dencooper.fracejob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FracejobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FracejobApplication.class, args);
+	}
+
+}
---
+++ backend/fracejob/src/test/java/vn/dencooper/fracejob/FracejobApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.dencooper.fracejob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FracejobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
