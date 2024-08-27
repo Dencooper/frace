5fd72cdaabc59ab755eb1d13fd1cc391b65dff64
namle <lephuongnam958506@gmail.com>
2024-08-27 23:46:00 +0700

    add and format exceptions

---
+++ backend/fracejob/src/main/java/vn/dencooper/fracejob/exception/ErrorCode.java
@@ -0,0 +1,22 @@
+package vn.dencooper.fracejob.exception;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public enum ErrorCode {
+    UNCATEGORIZED_EXCEPTION(500, "Uncategorized exception"),
+    INVALID_KEY(500, "Invalid message key"),
+    EMAIL_EXISTED(400, "Email đã tổn tại"),
+    EMAIL_INVALID(400, "Email không hợp lệ"),
+    PASSWORD_INVALID(400, "Mật khẩu phải có tối thiểu 6 kí tự"),
+    FULLNAME_INVALID(400, "Họ tên phải có tối thiếu 3 kí tự");
+
+    private int code;
+    private String message;
+
+}
--- backend/fracejob/src/main/java/vn/dencooper/fracejob/exception/GlobalExceptionHandler.java
+++ backend/fracejob/src/main/java/vn/dencooper/fracejob/exception/GlobalExceptionHandler.java
@@ -1,14 +1,53 @@
 package vn.dencooper.fracejob.exception;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import vn.dencooper.fracejob.domain.dto.ApiResponse;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(value = RuntimeException.class)
-    public ResponseEntity<String> handlingRuntimeException(RuntimeException exception) {
-        return ResponseEntity.badRequest().body(exception.getMessage());
+    @ExceptionHandler(value = Exception.class)
+    public ResponseEntity<ApiResponse> handlingUncaseException(RuntimeException exception) {
+        ApiResponse res = new ApiResponse<>();
+        res.setError("true");
+        res.setStatusCode(ErrorCode.UNCATEGORIZED_EXCEPTION.getCode());
+        res.setMessage(ErrorCode.UNCATEGORIZED_EXCEPTION.getMessage());
+
+        return ResponseEntity.badRequest().body(res);
     }
+
+    @ExceptionHandler(value = AppException.class)
+    public ResponseEntity<ApiResponse> handlingAppException(AppException exception) {
+        ApiResponse res = new ApiResponse<>();
+        ErrorCode errorCode = exception.getErrorCode();
+        res.setError("true");
+        res.setStatusCode(errorCode.getCode());
+        res.setMessage(errorCode.getMessage());
+        return ResponseEntity.badRequest().body(res);
+    }
+
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse> handlingMethodArgumentNotValidException(
+            MethodArgumentNotValidException exception) {
+
+        String enumKey = exception.getFieldError().getDefaultMessage();
+
+        ErrorCode errorCode = ErrorCode.INVALID_KEY;
+
+        try {
+            errorCode = ErrorCode.valueOf(enumKey);
+        } catch (IllegalArgumentException e) {
+        }
+
+        ApiResponse res = new ApiResponse<>();
+        res.setError("true");
+        res.setStatusCode(errorCode.getCode());
+        res.setMessage(errorCode.getMessage());
+        return ResponseEntity.badRequest().body(res);
+    }
+
 }
